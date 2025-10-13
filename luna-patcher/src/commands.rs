0822340b7ec6c1c7a38d0354aea65d57fbb2a966
YannickMyxe <shawn.froste.sf@gmail.com>
2025-10-13 17:11:49 +0200

    Updated to subcommands structure

--- luna-patcher/src/commands.rs
+++ luna-patcher/src/commands.rs
@@ -5,14 +5,18 @@ use clap::Parser;
 #[command(name = "luna-patcher")]
 #[command(about = "Created versions/patches based on your file changes", version = "1.0")]
 pub struct Cli {
-    /// The command to execute
-    #[arg(short, long)]
-    pub command: String,
+    #[command(subcommand)]
+    command: Commands,
 }
 
-pub fn run(args: Cli) {
-    match args.command.as_str() {
-        "example" => println!("Running example command"),
-        _ => println!("Unknown command: {}", args.command),
-    }
+#[derive(clap::Subcommand)]
+enum Commands {
+    /// An example command
+    Example,
+}
+
+pub fn run(cli: Cli) {
+    match &cli.command {
+        Commands::Example => println!("Running example command"),
+    };
 }
