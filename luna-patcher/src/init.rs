aca60dc1d835a63a4f4d50205b78a253224a55e3
YannickMyxe <shawn.froste.sf@gmail.com>
2025-10-13 19:28:37 +0200

    Added reset on when file exists

--- luna-patcher/src/init.rs
+++ luna-patcher/src/init.rs
@@ -29,22 +29,30 @@ fn print(disabled: &Vec<String>, mods: &Vec<String>, output: String) {
 
     let output = dir.to_owned() + "/" + &output.clone();
 
+    if !fs::metadata(output.clone()).is_ok() {
+        // Create the file if it does not exist
+        std::fs::File::create(output.clone()).expect("Failed to create output file");
+    } else {
+        std::fs::remove_file(output.clone()).expect("Failed to remove file");
+        std::fs::File::create(output.clone()).expect("Failed to create output file");
+    }
+
     let mut data_file = OpenOptions::new()
         .append(true)
         .open(output.clone())
-        .expect("cannot open file");
+        .expect("Cannot open file");
 
-    write!(&mut data_file, "# Lunala Patcher \n").expect("Failed to write Header to file");
+    write!(&mut data_file, "# Lunala Patcher\n").expect("Failed to write Header to file");
 
     if !disabled.is_empty() {
-        write!(&mut data_file, "\n## Optional Files\n").expect("Failed to write section header to file");
+        write!(&mut data_file, "\n## Optional Files\n\n").expect("Failed to write section header to file");
         for file_name in disabled {
             write!(&mut data_file, "{}", format!(" -[_] {}\n", file_name))
                 .expect("Failed to write to file, failed to add disabled files");
         }
     }
     if !mods.is_empty() {
-        write!(&mut data_file, "\n## Mod Files\n").expect("Failed to write section header to file");
+        write!(&mut data_file, "\n## Mod Files'\n\n").expect("Failed to write section header to file");
         for file_name in mods {
             write!(&mut data_file, "{}", format!(" -[x] {}\n", file_name))
                 .expect("Failed to write to file, failed to add mod files");
