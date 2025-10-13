ba61fba9511541205e0995732e3dda07f75db316
YannickMyxe <shawn.froste.sf@gmail.com>
2025-10-13 18:15:33 +0200

    Destinguish between file types

--- luna-patcher/src/patch.rs
+++ luna-patcher/src/patch.rs
@@ -2,5 +2,10 @@ pub fn patch(files: Vec<String>) {
     println!("Patching files...");
     for file in files {
         println!("File: {}", file);
+        match file.split(".").last() {
+            Some("disabled") => println!(" - This file is disabled"),
+            Some(_) => println!(" - This is a regular file"),
+            None => println!(" - Unknown file type"),
+        }
     }
 }
